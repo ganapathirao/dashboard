618db49d6fcdae05aa6505ed7664c5d7fff9604a
ganapathirao <31933079+ganapathirao@users.noreply.github.com>
2018-03-28 02:00:15 +0530

    changed

---
+++ assets/jQuery/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$(".badge1").click(function(){
+
+		$("#messages-dropdown").fadeToggle();
+
+	});
+
+	$(".badge2").click(function(){
+
+		$("#notifications-dropdown").fadeToggle();
+
+	});
+
+	$(".badge3").click(function(){
+
+		$("#tasks-dropdown").fadeToggle();
+
+	});
+
+
+});
