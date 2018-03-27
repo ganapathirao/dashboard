4721f4d7da6a1bff1304ac938617fd3f91ccb94d
ganapathirao <31933079+ganapathirao@users.noreply.github.com>
2018-03-27 19:25:47 +0530

    assets

---
+++ assets/jquery/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	$(".dropdown").click(function(){
+		$(".dropdown-content").fadeToggle();
+	});
+
+	$(".bell-dropdown").click(function(){
+		$(".bell-dropdown-content").fadeToggle();
+	});
+
+	$(".flag-dropdown").click(function(){
+		$(".flag-dropdown-content").fadeToggle();
+	});
+
+});
