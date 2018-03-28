f14d514bf71d871997bc5a760fcef2ff1517cb17
ganapathirao <31933079+ganapathirao@users.noreply.github.com>
2018-03-28 19:48:31 +0530

    updated

--- assets/jQuery/script.js
+++ assets/jQuery/script.js
@@ -1,21 +1,42 @@
 $(document).ready(function(){
+	
 	$(".badge1").click(function(){
-
+		$("#notifications-dropdown").fadeOut();
+		$("#tasks-dropdown").fadeOut();
+		$("#user-dropdown").fadeOut();
 		$("#messages-dropdown").fadeToggle();
-
 	});
 
 	$(".badge2").click(function(){
-
-		$("#notifications-dropdown").fadeToggle();
-
+		$("#messages-dropdown").fadeOut();
+		$("#tasks-dropdown").fadeOut();
+		$("#user-dropdown").fadeOut();
+		$("#notifications-dropdown").fadeToggle(); 
 	});
 
 	$(".badge3").click(function(){
-
+		$("#notifications-dropdown").fadeOut();
+		$("#messages-dropdown").fadeOut();
+		$("#user-dropdown").fadeOut();
 		$("#tasks-dropdown").fadeToggle();
-
 	});
 
+	$(".user-name").click(function(){
+		$("#notifications-dropdown").fadeOut();
+		$("#messages-dropdown").fadeOut();
+		$("#tasks-dropdown").fadeOut();
+		$("#user-dropdown").fadeToggle();
+	});
+
+	$(".toggle-side-nav").click(function(){
+	    $(".side-nav").animate({
+	      height: "toggle"
+	    });
+	});
+
+	 $("#charts-toggle").click(function(){
+		$(".charts-childs").fadeToggle();
+		$("i", this).toggleClass("fa fa-angle-down fa fa-angle-left");
+	});
 
 });
